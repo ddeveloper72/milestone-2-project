11cd10e5f7953f35929b5f078c52a39142cea5d7
ddeveloper72 <duncan.niamh@gmail.com>
2018-05-15 15:44:37 +0100

    Graph 1 - Staff Members Started

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,9 +1,33 @@
 queue()
-        .defer(d3.json, "data/patient_visits.json")
+        .defer(d3.json, "data/clinics.json")
         .await(makeGraphs);
 
 //*************************************************************************************************
 // Cross filter Function   
 function makeGraphs(error, clinicData){
+        var ndx = crossfilter(clinicData);
+
+        show_staff_balance(ndx);
+
+        dc.renderAll();
+
+}
+
+function show_staff_balance(ndx){
+        var dim = ndx.dimension(dc.pluck('staff'));
+        var group = dim.group();
+
+        dc.barChart("#staff-profile")
+                .width(350)
+                .height(250)
+                .margins({top: 10, right: 50, bottom: 30, left: 50})
+                .dimension(dim)
+                .group(group)
+                .transitionDuration(500)
+                .x(d3.scale.ordinal())
+                .xUnits(dc.units.ordinal)
+                .xAxisLabel("Staff")
+                .yAxisLabel("Number of Staff")
+                .yAxis().ticks(20);
 
 }
